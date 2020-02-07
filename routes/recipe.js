3f7fb047a534b2276dfc2ba2f8a31f1f551b1a32
diazkautsar <diazkautsar77@gmail.com>
2020-02-07 07:06:58 +0700

    find recipe

--- routes/recipe.js
+++ routes/recipe.js
@@ -2,5 +2,6 @@ const router = require("express").Router()
 const recipeController = require("../controllers/recipeController")
 
 router.post('/recipes', recipeController.create)
+router.get('/recipes', recipeController.findRecipe)
 
 module.exports = router
